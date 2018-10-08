b24310d349317e5b387149631772ddb96b44e72c
Rtstl Developer <rtstldeveloper@rtstls-macbook-air.local>
2018-10-08 10:31:56 +0530

    Initial Commit

---
+++ PetVet/RegitserViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  life
+//
+//  Created by Rtstl Developer on 06/07/18.
+//  Copyright © 2018 Ahmed Sahib. All rights reserved.
+//
+
+import UIKit
+
+class RegitserViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ PetVet/UIview+Extention.swift
@@ -0,0 +1,42 @@
+//
+//  UIview+Extention.swift
+//  life
+//
+//  Created by Rtstl Developer on 06/07/18.
+//  Copyright © 2018 Ahmed Sahib. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+@IBDesignable extension UIView {
+    @IBInspectable var borderColor: UIColor? {
+        set {
+            layer.borderColor = newValue?.cgColor
+        }
+        get {
+            guard let color = layer.borderColor else {
+                return nil
+            }
+            return UIColor(cgColor: color)
+        }
+    }
+    @IBInspectable var borderWidth: CGFloat {
+        set {
+            layer.borderWidth = newValue
+        }
+        get {
+            return layer.borderWidth
+        }
+    }
+    @IBInspectable var cornerRadius: CGFloat {
+        set {
+            layer.cornerRadius = newValue
+            clipsToBounds = newValue > 0
+        }
+        get {
+            return layer.cornerRadius
+        }
+    }
+}
+
